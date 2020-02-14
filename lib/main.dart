0fe2fe62e884242634a80eef937d0a17fa7dc97a
rendra prames <rendraprames@gmail.com>
2020-02-14 14:10:02 +0700

    Tugas landing page

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp( new MaterialApp(
+  home: new LandingPage(),
+)); //MaterialApp
+}
+
+class LandingPage extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+  return new Scaffold(
+   body: 
+   new Center(
+     child:  new Text("Selamat Datang di UTY"),
+     )
+  );//Scaffold
+  }
+}
